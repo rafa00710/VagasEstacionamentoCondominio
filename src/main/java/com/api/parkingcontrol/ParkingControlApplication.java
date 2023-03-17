3585f0f4e23f3140d7547a69efd98d1af54f3358
Rafael Pereira de Lima <rafael_pereira_007@hotmail.com>
2023-03-17 09:13:12 -0300

    Criação das vagas de condominio.

---
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -0,0 +1,21 @@
+package com.api.parkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController // Bean do tipo controller
+public class ParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlApplication.class, args);
+	}
+
+	@GetMapping("/") // Para acessar o http 8080
+	public String index(){
+		return "Olá Mundo";
+	}
+
+}
